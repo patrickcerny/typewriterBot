7fd51d43d888d7cd91fd3b6e0162f60df453b7ce
Patrick Cerny <patrick.cerny04@gmail.com>
2024-12-04 01:56:24 +0100

    v2 of typewriter bot (only chrome working)

--- typewriterbot.py
+++ typewriterbot.py
@@ -1,199 +1,178 @@
 import sys
-import os
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from pynput.keyboard import Controller, Key
-import keyboard
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from selenium.webdriver.edge.service import Service as EdgeService
+from pynput.keyboard import Controller
 import time
-import geckodriver_autoinstaller
-from webdriverdownloader import GeckoDriverDownloader
-
-gdd = GeckoDriverDownloader()
-gdd.download_and_install()
-
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.webdriver import WebDriver
+from webdriver_manager.chrome import ChromeDriverManager
 
 keyboardpy = Controller()
 
 errorList = [
     "Invalid Arguments",
-    "connection refused, check internet connection"
+    "Connection refused, check internet connection"
 ]
-
 
 def Exit(Error, errorMessage):
     if Error:
-        print("Error! something went wrong!")
+        print("Error! Something went wrong!")
         print("Error Message: " + errorMessage)
-
-    print("Press any key to exit!")
+    print("Press Enter to exit.")
     input()
     sys.exit(0)
 
-# function for loggin into an account and starting the newest lesson
 def Login():
-    print("Hello User!\nThis is a warning! This application has to use your typetrainer username and password! " +
-          "The credentials will not be saved in any way. They serve for login purpose only. " +
-          "Do you want to continue) (y/n)")
+    print(
+        "Hello User!\nThis is a warning! This application has to use your typetrainer username and password! "
+        "The credentials will not be saved in any way. They serve for login purposes only.\n"
+        "Do you want to continue? (y/n)"
+    )
     answer = input().strip().lower()
 
     if answer == "n":
         Exit(False, "")
 
     print("Please Enter your Username:")
-    username = input()
-    print("Please enter your Password")
-    password = input()
+    username = input().strip()
+    print("Please Enter your Password:")
+    password = input().strip()
 
     if answerBrowser == "f":
-        driver = webdriver.Firefox()
+        driver = webdriver.Firefox(service=FirefoxService(executable_path="./driver/geckodriver"))
     elif answerBrowser == "c":
-        driver = webdriver.Chrome("driver\chromedriver.exe")
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     elif answerBrowser == "e":
-        driver = webdriver.Edge("driver\msedgedriver.exe")
+        driver = webdriver.Edge(service=EdgeService("./driver/msedgedriver"))
     else:
         Exit(True, errorList[0])
 
     try:
-      driver.get("https://at4.typewriter.at/index.php?r=site/index")
-      driver.minimize_window()
-      driver.maximize_window()
-      
+        driver.get("https://at4.typewriter.at/index.php?r=site/index")
+        driver.maximize_window()
+        time.sleep(1)
     except Exception as e:
-      Exit(True, str(e))
+        Exit(True, str(e))
 
-
-    
-
-    # login in homepage
     try:
-      formLogin_un = driver.find_element_by_id("LoginForm_username")
-      formLogin_pw = driver.find_element_by_id("LoginForm_pw")
-      formLogin_submit = driver.find_element_by_name("yt0")
+        formLogin_un = driver.find_element(By.ID, "LoginForm_username")
+        formLogin_pw = driver.find_element(By.ID, "LoginForm_pw")
+        formLogin_submit = driver.find_element(By.NAME, "yt0")
     except Exception as e:
-      Exit(True, str(e))
+        Exit(True, str(e))
 
-      
-    
+    time.sleep(1)   
 
-    # paste in user-credentials
     formLogin_un.send_keys(username)
     formLogin_pw.send_keys(password)
     formLogin_submit.click()
 
     time.sleep(1)
-
-    
     return driver
 
-
 def NextLesson(driver):
-  # opens link to next lesson
-  time.sleep(1)
-  linkToLesson = driver.find_element_by_class_name("cockpitStartButton")
-  linkToLesson.click()
-# function for doing an excercise on typewriter
-
-
-def DoExcercise(driver):
-
     time.sleep(1)
     try:
-      # gets first char of text
-      currentChar = driver.find_element_by_id("actualLetter").text
+        linkToLesson = driver.find_element(By.CLASS_NAME, "cockpitStartButton")
+        linkToLesson.click()
     except Exception as e:
-      Exit(True, str(e))
-    
-    # starts the lesson
+        Exit(True, str(e))
+
+def DoExercise(driver):
     keyboardpy.press(Keys.ENTER)
-    time.sleep(0.5)
-    # types the first char again for update of #remainingText
-
-    keyboardpy.press(currentChar)
-
-    # gets remaining text + currentchar
+    time.sleep(1)
     try:
-      remainingText = driver.find_element_by_id("remainingText").text
-      currentChar = driver.find_element_by_id("actualLetter").text
+        box = driver.find_element(By.ID, "text_todo_1")
+        currentChar = box.find_element(By.TAG_NAME, "span").text
     except Exception as e:
-      Exit(True, str(e))
-      
-    # types the 2nd char
-    time.sleep(0.5)
+        Exit(True, str(e))
+
+    # remove checkbox
+    keyboardpy.press(Keys.ENTER)
+    time.sleep(2.5)
+    
+    #find first key
+    currentChar = box.find_element(By.TAG_NAME, "span").text
     keyboardpy.press(currentChar)
+    time.sleep(2.5)
 
+    amountRemaining = driver.find_element(By.ID, "amountRemaining").text
+    remainingInt = int(amountRemaining)
 
+    while remainingInt > 0:
+        # Locate the input box
+        box = driver.find_element(By.ID, "text_todo_1")
+        # Get the current character from the span inside the box
+        currentChar = box.find_element(By.TAG_NAME, "span").text
+        # Simulate typing the current character
+        keyboardpy.press(currentChar)
+        time.sleep(60 / int(answerSpeed))
+        # Update the remaining count
+        amountRemaining = driver.find_element(By.ID, "amountRemaining").text
+        remainingInt = int(amountRemaining)
 
-    # types every char in the remaining Text
-    for char in remainingText:
-        keyboardpy.press(char)
-        time.sleep(60/int(answerSpeed))
+def GotoHomeScreen(driver):
+    try:
+        driver.get("https://at4.typewriter.at/index.php?r=user/overview")
+        time.sleep(5)
+    except Exception as e:
+        Exit(True, str(e))
 
-def GotoHomescreen(driver):
-  driver.get("https://at4.typewriter.at/index.php?r=user/overview")
-  time.sleep(5)
+print("TypeWriterBot by Patrick Cerny, Github: https://github.com/patrickcerny\n")
 
-print(".")
-print(".")
-print(".")
-print(".")
-print("TypeWriterBot by Patrick Cerny, Github: https://github.com/patrickcerny")
-print(".")
-print(".")
-print(".")
-
-print("What Browser do you use? (Firefox: F | Chrome: C | Edge: E")
+print("What browser do you use? (Firefox: F | Chrome: C | Edge: E)")
 answerBrowser = input().strip().lower()
-if answerBrowser != "e" and answerBrowser != "f" and answerBrowser != "c":
+if answerBrowser not in ["f", "c", "e"]:
     Exit(True, errorList[0])
 
+print("Do you want to log in and do your next exercise, or just do an exercise without login? (Login: L | Exercise: E)")
+answer = input().strip().lower()
 
-print("Do you want to login and do your next Exercise or just do a Excercise without login? (Login: L | Excerice: E)")
-answer = input().strip().lower() 
-
-print("What speed to you want to type in? (f. e. 300 => 300 chars / min - [It will always finish a bit slower then the speed you type in!])")
+print("What speed do you want to type in? (e.g., 300 => 300 chars/min)")
 try:
-  answerSpeed = int(input())
+    answerSpeed = int(input().strip())
 except Exception as e:
-  Exit(True, str(e))
-  
+    Exit(True, str(e))
+
 if answer == "l":
-  print("How many excercises do you want to do? (1 to How-Many-You-Want)")
-  try:
-    timesExcercise = int(input())
-  except Exception as e:
-    Exit(True, e)
+    print("How many exercises do you want to do? (1 or more)")
+    try:
+        timesExercise = int(input().strip())
+    except Exception as e:
+        Exit(True, str(e))
 
-  driver = Login()
-  for i in range(timesExcercise):
-    NextLesson(driver)
-    DoExcercise(driver)
-    time.sleep(1)
-    GotoHomescreen(driver)
-    
+    driver = Login()
+    for _ in range(timesExercise):
+        NextLesson(driver)
+        DoExercise(driver)
+        time.sleep(1)
+        GotoHomeScreen(driver)
+
 elif answer == "e":
-    print("Please provide the URL of the excercise:")
-    url = input()
+    print("Please provide the URL of the exercise:")
+    url = input().strip()
     try:
-      if answerBrowser == "f":
-          driver = webdriver.Firefox(".\driver\geckodriver.exe")
-      elif answerBrowser == "c":
-          driver = webdriver.Chrome(".\driver\chromedriver.exe")
-      elif answerBrowser == "e":
-          driver = webdriver.Edge(".\driver\msedgedriver.exe")
+        if answerBrowser == "f":
+            driver = webdriver.Firefox(service=FirefoxService(executable_path="./driver/geckodriver"))
+        elif answerBrowser == "c":
+            driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        elif answerBrowser == "e":
+            driver = webdriver.Edge(service=EdgeService("./driver/msedgedriver"))
+        else:
+            Exit(True, errorList[0])
+
+        driver.get(url)
+        driver.maximize_window()
     except Exception as e:
-      Exit(True, e)
-    
-    try:
-      driver.get(url)
-      driver.maximize_window()
-      driver.switch_to.window(driver.current_window_handle)
-    except Exception as e:
-      Exit(True, e)
-    
-    DoExcercise(driver)
+        Exit(True, str(e))
+
+    DoExercise(driver)
 else:
-  Exit(True, errorList[0])
+    Exit(True, errorList[0])
 
-
-print("Thank you for using my bot! If you have any feedback, please contact me over my github https://github.com/patrickcerny")
-
+print("Thank you for using my bot! If you have any feedback, please contact me on GitHub: https://github.com/patrickcerny")
